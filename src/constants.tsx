a1e7cfd5663ae3cd2edf243d13ad9bda36244571
eithen504 <eithen504@gmail.com>
2026-02-05 16:24:11 +0530

    some changes

--- src/constants.tsx
+++ src/constants.tsx
@@ -7,120 +7,120 @@ import { AccountfilledIcon, AccountIcon, CompactGridIcon, CompactListIcon, Defau
 import type { HomePageTabs, NavigationItem, ViewComponent, ViewIcon, ViewSkelton, ShowPageTabs, LibrarySortOptionsMap, GenresMap } from "./types";
 
 const GENRES = [
-  // {
-  //   id: "3ZJr7yHSWEdqUfnIPpASXo",
-  //   title: "Summer",
-  //   bgColor: "#27856A",
-  //   image: "https://i.scdn.co/image/ab67fb8200005caf1d858b423c5b3194d72c7c28"
-  // },
-  // {
-  //   id: "1KLPoBkeHXzq3NaGMyvCYx",
-  //   title: "Love",
-  //   bgColor: "#DC148C",
-  //   image: "https://i.scdn.co/image/ab67fb8200005caf21c9a95a2702ce535fb07915"
-  // },
-  // {
-  //   id: "7MqAxb9kT8t2w70zkTqRj5",
-  //   title: "Mood",
-  //   bgColor: "#B31048",
-  //   image: "https://i.scdn.co/image/ab67fb8200005cafe542e9b59b1d2ae04b46b91c"
-  // },
-  // {
-  //   id: "0PLU8u8MyjYbV5uWiRjHte",
-  //   title: "Party",
-  //   bgColor: "#8D67AB",
-  //   image: "https://i.scdn.co/image/ab67fb8200005caf0b0d0bfac454671832311615"
-  // },
-  // {
-  //   id: "6GKJrPQfutE3SmAMv6Vnrn",
-  //   title: "Decades",
-  //   bgColor: "#A56752",
-  //   image: "https://i.scdn.co/image/ab67fb8200005cafb7e805033eb938aa75d09336"
-  // },
-  // {
-  //   id: "5A9qKLA4R1tQhPnYH1k7vB",
-  //   title: "Dance/Electronic",
-  //   bgColor: "#477D95",
-  //   image: "https://i.scdn.co/image/ab67fb8200005caf26ada793217994216c79dad8"
-  // },
-  // {
-  //   id: "4RqvmyV7Wk3xNCePuFYvRh",
-  //   title: "Student",
-  //   bgColor: "#AF2896",
-  //   image: "https://i.scdn.co/image/ab67fb8200005cafdad1281e13697e8d8cf8f347"
-  // },
-  // {
-  //   id: "1Cvx9rtfJmZqB32TQHcuvV",
-  //   title: "Chill",
-  //   bgColor: "#B06239",
-  //   image: "https://i.scdn.co/image/ab67fb8200005caf330ca3a3bfaf8b18407fb33e"
-  // },
-  // {
-  //   id: "6FkaUzp9yrx4hqhVp6wS4a",
-  //   title: "Workout",
-  //   bgColor: "#777777",
-  //   image: "https://i.scdn.co/image/ab67fb8200005caf6af6d83c78493644c9b0627b"
-  // },
-  // {
-  //   id: "4DkfSna2hTbbYTuKe3gMwg",
-  //   title: "Sleep",
-  //   bgColor: "#1E3264",
-  //   image: "https://i.scdn.co/image/ab67fb8200005caf1cef0cee1e498abb8e74955f"
-  // },
-  // {
-  //   id: "7JZp2b9VcX8F2tMkwe1RyM",
-  //   title: "Instrumental",
-  //   bgColor: "#537AA1",
-  //   image: "https://i.scdn.co/image/ab67fb8200005caf44774504bdbe31a7bc45598c"
-  // },
-  // {
-  //   id: "2NKCcdSy8DaCgeuv5ZkWNb",
-  //   title: "At Home",
-  //   bgColor: "#5179A1",
-  //   image: "https://i.scdn.co/image/ab67fb8200005caf60ad5f3ea4988eff993d5e1a"
-  // },
-  // {
-  //   id: "7jNQXo7S7HB5zZqRneMx28",
-  //   title: "Classical",
-  //   bgColor: "#7D4B32",
-  //   image: "https://i.scdn.co/image/ab67fb8200005caf4597370d1058e1ec3c1a56fa"
-  // },
-  // {
-  //   id: "0xJtDdgA1id36SjaqTlwBt",
-  //   title: "Folk & Acoustic",
-  //   bgColor: "#BC5900",
-  //   image: "https://i.scdn.co/image/ab67fb8200005cafcc70a3c2e4c71398708bdc4a"
-  // },
-  // {
-  //   id: "5mK7Zut2Y9kZye8gPNgm7v",
-  //   title: "Focus",
-  //   bgColor: "#A56752",
-  //   image: "https://i.scdn.co/image/ab67fb8200005caf9a27506d5dde68b9da373196"
-  // },
-  // {
-  //   id: "4r9Gw7mGxHkXybjvxN2I6y",
-  //   title: "Children & Family",
-  //   bgColor: "#4D96E2",
-  //   image: "https://i.scdn.co/image/ab67fb8200005cafe72590f91baff169b1595ab4"
-  // },
-  // {
-  //   id: "7iNFAUtQtr2oAc16p6DqCk",
-  //   title: "Cooking & Dining",
-  //   bgColor: "#BA5D07",
-  //   image: "https://i.scdn.co/image/ab67fb8200005cafacbcb985978c387411d69e4f"
-  // },
+  {
+    id: "3ZJr7yHSWEdqUfnIPpASXo",
+    title: "Summer",
+    bgColor: "#27856A",
+    image: "https://i.scdn.co/image/ab67fb8200005caf1d858b423c5b3194d72c7c28"
+  },
+  {
+    id: "1KLPoBkeHXzq3NaGMyvCYx",
+    title: "Love",
+    bgColor: "#DC148C",
+    image: "https://i.scdn.co/image/ab67fb8200005caf21c9a95a2702ce535fb07915"
+  },
+  {
+    id: "7MqAxb9kT8t2w70zkTqRj5",
+    title: "Mood",
+    bgColor: "#B31048",
+    image: "https://i.scdn.co/image/ab67fb8200005cafe542e9b59b1d2ae04b46b91c"
+  },
+  {
+    id: "0PLU8u8MyjYbV5uWiRjHte",
+    title: "Party",
+    bgColor: "#8D67AB",
+    image: "https://i.scdn.co/image/ab67fb8200005caf0b0d0bfac454671832311615"
+  },
+  {
+    id: "6GKJrPQfutE3SmAMv6Vnrn",
+    title: "Decades",
+    bgColor: "#A56752",
+    image: "https://i.scdn.co/image/ab67fb8200005cafb7e805033eb938aa75d09336"
+  },
+  {
+    id: "5A9qKLA4R1tQhPnYH1k7vB",
+    title: "Dance/Electronic",
+    bgColor: "#477D95",
+    image: "https://i.scdn.co/image/ab67fb8200005caf26ada793217994216c79dad8"
+  },
+  {
+    id: "4RqvmyV7Wk3xNCePuFYvRh",
+    title: "Student",
+    bgColor: "#AF2896",
+    image: "https://i.scdn.co/image/ab67fb8200005cafdad1281e13697e8d8cf8f347"
+  },
+  {
+    id: "1Cvx9rtfJmZqB32TQHcuvV",
+    title: "Chill",
+    bgColor: "#B06239",
+    image: "https://i.scdn.co/image/ab67fb8200005caf330ca3a3bfaf8b18407fb33e"
+  },
+  {
+    id: "6FkaUzp9yrx4hqhVp6wS4a",
+    title: "Workout",
+    bgColor: "#777777",
+    image: "https://i.scdn.co/image/ab67fb8200005caf6af6d83c78493644c9b0627b"
+  },
+  {
+    id: "4DkfSna2hTbbYTuKe3gMwg",
+    title: "Sleep",
+    bgColor: "#1E3264",
+    image: "https://i.scdn.co/image/ab67fb8200005caf1cef0cee1e498abb8e74955f"
+  },
+  {
+    id: "7JZp2b9VcX8F2tMkwe1RyM",
+    title: "Instrumental",
+    bgColor: "#537AA1",
+    image: "https://i.scdn.co/image/ab67fb8200005caf44774504bdbe31a7bc45598c"
+  },
+  {
+    id: "2NKCcdSy8DaCgeuv5ZkWNb",
+    title: "At Home",
+    bgColor: "#5179A1",
+    image: "https://i.scdn.co/image/ab67fb8200005caf60ad5f3ea4988eff993d5e1a"
+  },
+  {
+    id: "7jNQXo7S7HB5zZqRneMx28",
+    title: "Classical",
+    bgColor: "#7D4B32",
+    image: "https://i.scdn.co/image/ab67fb8200005caf4597370d1058e1ec3c1a56fa"
+  },
+  {
+    id: "0xJtDdgA1id36SjaqTlwBt",
+    title: "Folk & Acoustic",
+    bgColor: "#BC5900",
+    image: "https://i.scdn.co/image/ab67fb8200005cafcc70a3c2e4c71398708bdc4a"
+  },
+  {
+    id: "5mK7Zut2Y9kZye8gPNgm7v",
+    title: "Focus",
+    bgColor: "#A56752",
+    image: "https://i.scdn.co/image/ab67fb8200005caf9a27506d5dde68b9da373196"
+  },
+  {
+    id: "4r9Gw7mGxHkXybjvxN2I6y",
+    title: "Children & Family",
+    bgColor: "#4D96E2",
+    image: "https://i.scdn.co/image/ab67fb8200005cafe72590f91baff169b1595ab4"
+  },
+  {
+    id: "7iNFAUtQtr2oAc16p6DqCk",
+    title: "Cooking & Dining",
+    bgColor: "#BA5D07",
+    image: "https://i.scdn.co/image/ab67fb8200005cafacbcb985978c387411d69e4f"
+  },
   {
     id: "2d0pm4ytxytr5LRZkijPNJ",
     title: "Wellness",
     bgColor: "#148A08",
     image: "https://i.scdn.co/image/ab67fb8200005cafd4a8da930bccd56ebd7e48b0"
   },
-  // {
-  //   id: "4Pxr7y2ph4EBZCD5xG7kZJ",
-  //   title: "Travel",
-  //   bgColor: "#0D72ED",
-  //   image: "https://i.scdn.co/image/ab67fb8200005caf879a886d22672d9b5b987746"
-  // },
+  {
+    id: "4Pxr7y2ph4EBZCD5xG7kZJ",
+    title: "Travel",
+    bgColor: "#0D72ED",
+    image: "https://i.scdn.co/image/ab67fb8200005caf879a886d22672d9b5b987746"
+  },
   {
     id: "6Bxqye6sr9FofZy7Jj9eH2",
     title: "Nature & Noise",
